00cf1cf715cfe3fc7a2205d26f27cf1b7da91c60
aallen92 <aaron@aawebdev.co.uk>
2023-01-12 20:48:53 +0000

    moved firebase service account into environment variables

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,9 +1,20 @@
+require("dotenv").config();
 const firebase = require("firebase-admin");
 const { cert } = require("firebase-admin/app");
-const serviceAccount = require("./serviceAccountKey.json");
 
 const firebaseAppConfig = {
-  credential: cert(serviceAccount),
+  credential: cert({
+    type: process.env.FIREBASE_SA_TYPE,
+    project_id: process.env.FIREBASE_SA_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_SA_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_SA_PRIVATE_KEY,
+    client_email: process.env.FIREBASE_SA_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_SA_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_SA_AUTH_URI,
+    token_uri: process.env.FIREBASE_SA_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FIREBASE_SA_AUTH_PROVIDER_X509,
+    client_x509_cert_url: process.env.FIREBASE_SA_CLIENT_X509,
+  }),
 };
 
 const firebaseApp = firebase.initializeApp(firebaseAppConfig, "firebaseApp");
